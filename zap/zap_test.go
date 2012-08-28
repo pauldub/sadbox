07ee0fa0e3d12214c06c2ab62533acabdf00573d
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-08-28 16:46:26 -0300

    zap: a block test with multiple inheritance

--- zap/zap_test.go
+++ zap/zap_test.go
@@ -5,8 +5,58 @@
 package zap
 
 import (
+	"bytes"
 	"testing"
+	textTemplate "text/template"
+	htmlTemplate "html/template"
 )
 
-func TestZap(t *testing.T) {
+func TestBlock(t *testing.T) {
+	tpl := `
+	{{define "t1"}}foo-{{block "b1"}}t1b1-{{end}}bar{{end}}
+	{{define "t2" "t1"}}{{block "b1"}}t2b1-{{block "b2"}}t2b2-{{end}}{{end}}{{end}}
+	{{define "t3" "t2"}}{{block "b2"}}t3b2-{{end}}{{end}}
+	{{define "t4" "t2"}}{{block "b2"}}t4b2-{{end}}{{end}}
+	`
+	expect := map[string]string{
+		"t1": "foo-t1b1-bar",
+		"t2": "foo-t2b1-t2b2-bar",
+		"t3": "foo-t2b1-t3b2-bar",
+		"t4": "foo-t2b1-t4b2-bar",
+	}
+
+	zapper, err := new(Zapper).Parse(tpl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := new(bytes.Buffer)
+	if err := zapper.Zap(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	tpl = buf.String()
+	txt := textTemplate.Must(textTemplate.New("_").Parse(tpl))
+	htm := htmlTemplate.Must(htmlTemplate.New("_").Parse(tpl))
+
+	for name, value := range expect {
+		buf.Reset()
+		err = txt.ExecuteTemplate(buf, name, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result := buf.String()
+		if result != value {
+			t.Errorf("expected %q, got %q", value, result)
+		}
+
+		buf.Reset()
+		err = htm.ExecuteTemplate(buf, name, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result = buf.String()
+		if result != value {
+			t.Errorf("expected %q, got %q", value, result)
+		}
+	}
 }
