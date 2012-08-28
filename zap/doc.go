27893f92b8ce8ff6fe913af4422216027fe5a4a7
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-08-28 16:56:26 -0300

    zap: docs.

--- zap/doc.go
+++ zap/doc.go
@@ -13,7 +13,7 @@ Note: if you are familiar with Django templates, Jinja2, Mako, dust.js
 or many others, you already know the concept.
 
 Templates can include {{block}} actions that define replaceable template parts.
-We define a block just to mark a content as a placeholder. Like this:
+We define a block just to mark a content as a placeholder, like this:
 
 	{{define "base"}}
 	  <html>
@@ -34,19 +34,19 @@ We define a block just to mark a content as a placeholder. Like this:
 	{{end}}
 
 The idea is that we can reuse this same page layout in other templates, just
-replacing the "content" block. So let's do it. We  will need a template
-that "extends" the previous one. And here is how we declare it:
+replacing the "content" block. For this we will need a template that "extends"
+the previous one. Here is how we declare it:
 
 	{{define "tutorial" "base"}}
 	  ...
 	{{end}}
 
-See that the {{define}} action has two strings? The first is the name of the
-template being defined, and the second is the name of the template being
-extended. You can read it as "define tutorial extending base".
+The {{define}} action accepts a second string, which is the name of the
+template being extended. You can read it as "define tutorial extending base".
 
 A template that extends another one can do only one thing: define blocks
-to replace the ones from the base template.
+to replace the ones from the base template. Here we override the "content"
+block:
 
 	{{define "tutorial" "base"}}
 	  {{block "content"}}
